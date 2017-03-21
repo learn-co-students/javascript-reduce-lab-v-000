4044e3a4e7a176b030c6d48ee3466c896c35f817
Eddie Rossiter <eddie.rossiter@gmail.com>
2017-03-21 01:31:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+var totalBatteries = batteryBatches.reduce(function(total, num){
+  return total + num
+  }, 0)
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +20,48 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var wordCountMap = countWords(monologueLines)
+
+function countWords(array){
+  var string = ''
+  var wordCounts = []
+  var spacesCount = 1
+
+  for(let i = 0, l = array.length; i < l; i++) {
+    string = array[i]
+    for(let j = 0, k = string.length; j < k; j++) {
+      if(string[j] === ' ') {
+        spacesCount++
+      }
+    }
+    wordCounts[i] = spacesCount
+    spacesCount = 1
+    string = ''
+  }
+
+  var currentNumber = 0
+  var numberCount = 0
+  var currentWordCount = []
+  var result = {}
+
+
+  for(let i = 0, l = wordCounts.length; i < l; i++) {
+    currentNumber = wordCounts[i]
+
+    for (let j = 0; j < l; j++) {
+      if(wordCounts[j] === currentNumber) {
+        numberCount++
+      }
+    }
+
+    if(currentWordCount.includes(currentNumber) === false) {
+      currentWordCount.push(currentNumber)
+      var a = currentNumber.toString();
+      result[a] = numberCount
+    }
+
+    numberCount = 0
+  }
+  return result
+}
