fbfa5305026f53b254c9c7aa69e351ed3c7ece29
Christian Whitesides <whitesidescr@gmail.com>
2017-03-23 18:08:40 -0700

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+var totalBatteries = batteryBatches.reduce(
+  (acc, cur) => acc + cur,
+  0
+)
+
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +22,14 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var wordCountMap = monologueLines.reduce(
+  (allLines, line) => {
+    if (line.split(' ').length.toString() in allLines) {
+      allLines[line.split(' ').length.toString()]++
+    } else {
+      allLines[line.split(' ').length.toString()] = 1
+    }
+    return allLines
+  }, {}
+)
