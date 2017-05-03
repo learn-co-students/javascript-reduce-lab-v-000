aacea744f9ce81ebb11a19b2dbbc42e647dbe5b3
WilliamMena <williammena1991@gmail.com>
2017-05-03 16:21:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+const totalBatteries = batteryBatches.reduce(addBateries, 0)
+
+function addBateries(total, number) {
+  return total + number;
+}
+
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +23,43 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+
+// var wordCountMap = monologueLines.reduce(function (count, line) { 
+//   let numKey = [line.split(' ').length]
+
+//   if (numKey in count) {
+//     count[numKey]++;
+//   }
+//   else {
+//     count[numKey] = 1;
+//   }
+//   return count;
+// }, {});
+
+
+
+
+
+
+
+
+
+// function countWords(total, line) {
+//   // return line.split(' ').length
+//   return { total[ line.split(' ').length ]: 1 }
+// }
+
+var wordCountMap = monologueLines.reduce(countWords, {})
+
+function countWords(total, line) {
+
+  if ([line.split(' ').length] in total) {
+    total[line.split(' ').length]++
+  }
+  else {
+    total[line.split(' ').length] = 1
+  }
+  return total
+}
+
