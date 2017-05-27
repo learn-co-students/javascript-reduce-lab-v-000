57ac05ffc19725db23fdd2cf526dca547832a8df
DanL12186 <danlrcn@hotmail.com>
2017-05-27 14:12:10 -0400

    Done.

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
-  'Do you know how much I make a year?',
+  'Do you know how super duper special I am?',
   'I mean, even if I told you, you wouldn\'t believe it.',
   'Do you know what would happen if I suddenly decided to stop going into work?',
   'A business big enough that it could be listed on the NASDAQ goes belly up.',
   'Disappears!',
-  'It ceases to exist without me.',
+  'I\'m incredibly special and super important.',
   'No, you clearly don\'t know who you\'re talking to, so let me clue you in.',
   'I am not in danger, Skyler.',
   'I am the danger.',
@@ -16,3 +16,20 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const totalBatteries = batteryBatches.reduce((javascript,sucks) => {
+  return javascript + sucks;
+})
+
+const wordCountMap = monologueLines.reduce((accumulator, currentString) => {
+
+  const stringLength = function(currentString) {                                //stringLength = function to count the current streng's word length
+    return currentString.split(' ').length;
+  }
+
+  var key = stringLength(currentString);                                        //key is set to the word length of the current string
+
+  key in accumulator ?  accumulator[key] += 1 : accumulator[key] = 1;           //if word length is found in accumulator (a hash), increment count,
+                                                                                //....else add new one to the hash
+  return accumulator;
+}, {});                                                                         //initial value parameter for reduce; sets accumulator to a hash
