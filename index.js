fc8e7c0ee0d718c2761da761d7e54370a10b89a3
Nadine Fischoff <nadine.khattak@gmail.com>
2017-07-12 17:03:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+var totalBatteries = batteryBatches.reduce(batteryCounter, 0)
+
+
+function batteryCounter(totalCount, num) {
+  return totalCount + num;
+}
+
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +24,15 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var wordCountMap = monologueLines.reduce((map, sentence) => {
+  const wordCount = sentence.split(' ').length;
+
+  if (!map[wordCount]) {
+    map[wordCount] = 0;
+  }
+
+  map[wordCount]++;
+
+  return map;
+}, {});
