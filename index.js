58d061d32a8704853c4f5f0b2ec5afa8c935995e
machen2 <victoriamachen@gmail.com>
2017-07-29 15:52:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+//var totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0)
+var totalBatteries = batteryBatches.reduce(callback, 0)
+
+function callback(total, batch) {
+  return total + batch
+}
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +23,20 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var wordCountMap = monologueLines.reduce(callbackWordCount, {})
+
+function callbackWordCount(object, sentence) { //object is the initialValue above, {}
+  let key = wordCount(sentence)
+
+  if(key in object) {
+    object[key]++
+  } else {
+    object[key] = 1
+  }
+  return object
+}
+
+function wordCount(sentence) {
+  return sentence.split(" ").length
+}
