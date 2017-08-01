1e026ebb3d471b8b98c1adf88cc3f54e2dc43975
Vincente Martinez <vinny@vinnymartinez.com>
2017-08-01 20:17:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+var totalBatteries = batteryBatches.reduce((total, element) => total += element, 0);
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +18,14 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+var wordCountMap = monologueLines.reduce((hash, element) => {
+  let wordCount = element.split(' ').length;
+  if (!!hash[wordCount]) {
+    hash[wordCount] += 1;
+  }
+  else {
+    hash[wordCount] = 1;
+  }
+  return hash;
+}, {});
