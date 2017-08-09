db252ae75e64654de44eea3801865e197cb93c46
Natasha Gitlin <natasha.gitlin8@gmail.com>
2017-08-08 21:24:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
+
+//OR
+//var totalBatteries = batteryBatches.reduce(function(total, batch) {
+//return total + batch
+// }, 0);
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +24,14 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const wordCountMap = monologueLines.reduce((map, sentence) => {
+  const wordCount = sentence.split(' ').length;
+  if (!map[wordCount]) {
+    map[wordCount] = 0;
+  }
+
+  map[wordCount]++;
+
+  return map;
+}, {});
