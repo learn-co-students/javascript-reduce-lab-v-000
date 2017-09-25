6b756d950c2abc04e3fdccfe7a89c3268fb9b7e2
Jdville03 <jarrel.delottinville@gmail.com>
2017-09-25 00:43:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+/* var totalBatteries = batteryBatches.reduce(function (a, b) {
+  return a + b;
+}, 0); */
+
+const totalBatteries = batteryBatches.reduce(
+  (acc, cur) => acc + cur,
+  0
+);
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +25,12 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+const wordCountMap = monologueLines.reduce((map, sentence) => {
+  const wordCount = sentence.split(' ').length;
+  if (!map[wordCount]) {
+    map[wordCount] = 0;
+  }
+  map[wordCount]++;
+  return map;
+}, {});
