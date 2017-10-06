d3487e7b7a189511dbc62d61d3dc895a37e63079
James Fernandez <jffernan@comcast.net>
2017-10-06 14:16:10 -0700

    Re-do & added pseudocode // comments

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+/* 
+Create a new variable called totalBatteries, which is the sum of all of the battery counts in the batteryBatches array. 
+Note: Array.prototype.reduce() takes two arguments: the callback and the initialValue.
+*/
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const totalBatteries = batteryBatches.reduce((total, batch) => total + batch);
+console.log(totalBatteries);
 
+/* 
+Create a new variable called wordCountMap that reduces the monologueLines to a map that resembles the output as shown { '7': 2, '9': 1 }.
+*/
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -17,15 +26,13 @@ const monologueLines = [
   'I am the one who knocks!'
 ];
 
-const totalBatteries = batteryBatches.reduce((total, batch) => total + batch, 0);
 const wordCountMap = monologueLines.reduce((map, sentence) => {
   const wordCount = sentence.split(' ').length;
-
   if (!map[wordCount]) {
     map[wordCount] = 0;
   }
-
   map[wordCount]++;
-
   return map;
 }, {});
+
+console.log(wordCountMap);
